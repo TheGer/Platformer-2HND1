33b1a54c4d62b4dc531aab9658afb419d3ed5a29
Gerry Said <gerrysaid@gmail.com>
2014-03-24 09:52:13 +0100

    Animation controlled by code
    Animation controlled by code.  State changes through code.

---
+++ Assets/Scripts/playerController.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+//declare an animator object
+private var anim:Animator;
+
+function Start () {
+	//get the animation component of the player at the START of the game
+	anim = GetComponent(Animator);
+}
+
+function Update () {
+	//by default, stay in idle state
+	anim.SetInteger("MovingLeft",0);
+	anim.SetInteger("MovingRight",0);
+	
+	//pressing left arrow moves the character to the left
+	if (Input.GetAxis("Horizontal") < 0)
+	{
+		anim.SetInteger("MovingLeft",1);
+		
+	}
+	
+	if (Input.GetAxis("Horizontal") > 0)
+	{
+		anim.SetInteger("MovingRight",1);
+		
+	}
+
+}
